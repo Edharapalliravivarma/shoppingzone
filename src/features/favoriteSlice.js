b3732fcefb27ddac8046a4865187ac935d33624b
Edharapalliravivarma <ravivarma0018@gmail.com>
2026-06-15 20:08:15 +0530

    fix white page

--- src/features/favoriteSlice.js
+++ src/features/favoriteSlice.js
@@ -1,30 +1,36 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = JSON.parse(localStorage.getItem("favorites")) || [];
+const initialState =
+  JSON.parse(localStorage.getItem("favorites")) || [];
 
 const favoriteSlice = createSlice({
   name: "favorites",
   initialState,
   reducers: {
     addFavorite: (state, action) => {
-      const exists = state.find((item) => item.id === action.payload.id);
+      const exists = state.find(
+        (item) => item.id === action.payload.id
+      );
 
       if (!exists) {
         state.push(action.payload);
         localStorage.setItem("favorites", JSON.stringify(state));
-        alert("Added to favorites");
-      } else {
-        alert("Already in favorites");
       }
     },
 
     removeFavorite: (state, action) => {
-      const updated = state.filter((item) => item.id !== action.payload);
+      const updated = state.filter(
+        (item) => item.id !== action.payload
+      );
+
       localStorage.setItem("favorites", JSON.stringify(updated));
+
       return updated;
     }
   }
 });
 
-export const { addFavorite, removeFavorite } = favoriteSlice.actions;
+export const { addFavorite, removeFavorite } =
+  favoriteSlice.actions;
+
 export default favoriteSlice.reducer;
